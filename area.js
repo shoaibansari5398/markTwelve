773e076e2d022c9d9f83d0a6b7e987c7e0b2fc22
Shoaib Akhtar Ansari <shoaib.ansari5398@gmail.com>
2022-11-01 10:39:28 -0700

    styling complete

---
+++ area.js
@@ -0,0 +1,15 @@
+const base = document.querySelector(".base");
+const height = document.querySelector(".height");
+const output = document.querySelector("#output");
+const area_btn = document.querySelector("#area-btn");
+
+function calculateArea() {
+  if (Number(base.value) <= 0 || Number(height.value) <= 0) {
+    output.innerText = "Invalid data entred";
+    return;
+  }
+  const area = 0.5 * (base.value * height.value);
+  output.innerText = "The area of the triangle is " + area;
+}
+
+area_btn.addEventListener("click", calculateArea);
